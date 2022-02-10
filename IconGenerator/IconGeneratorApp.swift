fcb7d94f40553af778f7d2b6f31e69668e86944e
Stanley Pan <imoment.developer@gmail.com>
2022-02-10 16:18:37 -0500

    Initial Commit

---
+++ IconGenerator/IconGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconGeneratorApp.swift
+//  IconGenerator
+//
+//  Created by Stanley Pan on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct IconGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
